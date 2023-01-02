2f790866d0d292ed7841d4bcbd1bbbbcf7557651
AXEL FERNANDO MACEDO GUIMARAES <axelguimaraes2@gmail.com>
2023-01-02 19:31:32 +0000

    Added local node GUI;

--- src/main/java/edu/estg/localNode/LocalServer.java
+++ src/main/java/edu/estg/localNode/LocalServer.java
@@ -1,18 +1,77 @@
 package edu.estg.localNode;
 
+import edu.estg.localNode.UI.StartFrame;
 
-import edu.estg.localNode.UI.StartForm;
-
-import java.io.IOException;
+import java.io.*;
+import java.net.MulticastSocket;
+import java.net.Socket;
 import java.nio.channels.NotYetConnectedException;
 
-public class LocalServer {
-    private StartForm startForm;
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import static javax.swing.JOptionPane.showMessageDialog;
 
-    public LocalServer() {
+public class LocalServer {
+    private MulticastSocket multicastSocket;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private final StartFrame startFrame;
+
+    public LocalServer(MulticastSocket multicastSocket, String host, int port) throws IOException {
         try {
-            this.startForm = new StartForm(this);
-            this.startForm.configFrame();
+            this.socket = new Socket(host, port);
+            this.multicastSocket = multicastSocket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            this.startFrame = new StartFrame(this);
+            startFrame.configFrame();
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+            throw new NotYetConnectedException();
+        }
+    }
+
+    public void sendMessage(String message) {
+        new Thread(() -> {
+            if (message != null) {
+                try {
+                    bufferedWriter.write(message);
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+                } catch (IOException e) {
+                    closeEverything(socket, bufferedReader, bufferedWriter);
+                }
+            }
+        }).start();
+    }
+
+    public void receiveMessage() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    String responseMessage = bufferedReader.readLine();
+
+                    if (responseMessage == null) closeEverything(socket, bufferedReader, bufferedWriter);
+
+                    System.out.println(responseMessage);
+
+                    this.startFrame.processResponse(responseMessage);
+
+                    System.out.println(responseMessage);
+                } catch (IOException e) {
+                    closeEverything(socket, bufferedReader, bufferedWriter);
+                    break;
+                }
+            }
+        }).start();
+    }
+
+    private void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        try {
+            if (bufferedReader != null) bufferedReader.close();
+            if (bufferedWriter != null) bufferedWriter.close();
+            if (socket != null) socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -20,9 +79,11 @@ public class LocalServer {
 
     public static void main(String[] args) {
         try {
-            LocalServer localServer = new LocalServer();
-        } catch (NotYetConnectedException e) {
-            e.printStackTrace();
+            MulticastSocket multicastSocket = new MulticastSocket(4446);
+            LocalServer localServer = new LocalServer(multicastSocket, "localhost", 2048);
+            localServer.receiveMessage();
+        } catch (NotYetConnectedException | IOException e) {
+            showMessageDialog(null, "Not connected", "", ERROR_MESSAGE);
         }
     }
 }
---
+++ src/main/java/edu/estg/localNode/UI/StartFrame.java
@@ -0,0 +1,103 @@
+package edu.estg.localNode.UI;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import edu.estg.localNode.LocalServer;
+import edu.estg.models.LocalNode;
+import edu.estg.models.schemas.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class StartFrame extends JFrame {
+    private JTabbedPane tabbedPane1;
+    private JTextField loginUsernameField;
+    private JPasswordField loginPasswordField;
+    private JButton loginButton;
+    private JTextField registerNameField;
+    private JTextField registerUsernameField;
+    private JPasswordField registerPasswordField;
+    private JButton registerButton;
+    private JPanel mainPanel;
+    private final LocalServer localServer;
+    private MenuForm menuForm;
+    private Gson jsonHelper;
+
+    public StartFrame(LocalServer localServer) throws IOException {
+        this.jsonHelper = new GsonBuilder().serializeNulls().create();
+        this.localServer = localServer;
+        //configButtons();
+
+        this.localServer.sendMessage(this.jsonHelper.toJson(new Request<>(RequestType.REGISTER)));
+    }
+
+    private void configButtons() {
+        loginButton.addActionListener(e -> {
+            Login login = new Login(loginUsernameField.getText(), String.valueOf(loginPasswordField.getPassword()));
+            Request<Login> request = new Request<>(RequestType.LOGIN, login);
+            this.localServer.sendMessage(new Gson().toJson(request));
+        });
+
+        registerButton.addActionListener(e -> {
+            LocalNode localNode = new LocalNode(registerNameField.getText(), registerUsernameField.getText(), String.valueOf(registerPasswordField.getPassword()));
+            LocalNodeRegister localNodeRegister = new LocalNodeRegister(localNode);
+
+            Request<LocalNodeRegister> request = new Request<>(RequestType.REGISTER, localNodeRegister);
+            this.localServer.sendMessage(new Gson().toJson(request));
+        });
+    }
+
+    private void clearVariables() {
+        registerNameField.setText("");
+        registerUsernameField.setText("");
+        registerPasswordField.setText("");
+
+        loginUsernameField.setText("");
+        loginPasswordField.setText("");
+    }
+
+    public void configFrame() {
+        setContentPane(mainPanel); // TODO: mainPanel comes as null for some FUCKING REASON!!!!
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
+        setPreferredSize(new Dimension(600, 500));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+
+    public void setPreferredSize(Dimension dimension) {
+        super.setPreferredSize(dimension);
+    }
+
+    public synchronized void processResponse(String message) {
+        Response<Object> response = jsonHelper.<Response<Object>>fromJson(message, Response.class);
+
+        if (response.getStatus() != ResponseStatus.OK) {
+            showMessageDialog(new JDialog(), response.getMessage(), "", ERROR_MESSAGE);
+            return;
+        }
+
+        switch (response.getType()) {
+            case LOGIN:
+                LocalNodeLogin localNodeLogin = this.jsonHelper.<Response<LocalNodeLogin>>fromJson(message, new TypeToken<Response<LocalNodeLogin>>() {
+                }.getType()).getData();
+
+                //this.menuForm = new MenuForm(this, this.localServer, localNodeLogin.getLocalNode());
+                //this.menuForm.configFrame();
+        }
+    }
+}
